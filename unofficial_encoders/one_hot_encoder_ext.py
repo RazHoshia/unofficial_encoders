2e2801641cd4b330aead79409d04f8d2599b5a04
razhoshia <razhoshia@gmial.com>
2021-04-28 18:10:09 +0300

    add tox

--- unofficial_encoders/one_hot_encoder_ext.py
+++ unofficial_encoders/one_hot_encoder_ext.py
@@ -1,9 +1,8 @@
-from copy import copy
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder
 
-from unofficial_encoders.label_encoder_ext import LabelEncoderExt
+from .label_encoder_ext import LabelEncoderExt
 
 
 class OneHotEncoderExt(BaseEstimator, TransformerMixin):
