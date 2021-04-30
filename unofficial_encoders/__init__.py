5e8a34a318fc27cd37881b9ec6202554acce2a57
razho <razhoshia@gmail.com>
2021-04-30 20:00:36 +0300

    add null handler

--- unofficial_encoders/__init__.py
+++ unofficial_encoders/__init__.py
@@ -1,2 +1,7 @@
+import logging
+from logging import NullHandler
+
 from .label_encoder_ext import LabelEncoderExt  # noqa: F401
 from .one_hot_encoder_ext import OneHotEncoderExt  # noqa: F401
+
+logging.getLogger(__name__).addHandler(NullHandler())
